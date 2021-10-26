517f3fa6ee997f13168d7b01531a4059176adab3
Awesome <73072931+AwesomeDude357@users.noreply.github.com>
2021-10-26 13:21:31 -0400

    added all the basis for the item management (add item, remove item, initial item, and figuring out the date)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ucf.assignments {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.sql;
 
 
     opens ucf.assignments to javafx.fxml;
--- src/main/java/ucf/assignments/ToDoListItemManagement.java
+++ src/main/java/ucf/assignments/ToDoListItemManagement.java
@@ -2,19 +2,38 @@ package ucf.assignments;
 
 import javafx.collections.ObservableList;
 
+import java.sql.Date;
+
 /*
  *  UCF COP3330 Fall 2021 Assignment 4 Solution
  *  Copyright 2021 Rithvik Dinesh
  */
 
 public class ToDoListItemManagement {
-    public static ObservableList<ToDoListItem> addItem(String desc, String date, String status, ObservableList<ToDoListItem> theList) {
-        //Will take in the incoming values and create an item
-        //Will add the item to the ObservableList of items
-        return theList;
+    public static Date time() {
+        long millis=System.currentTimeMillis();
+        java.sql.Date date=new java.sql.Date(millis);
+        return date;
     }
 
-    public static ObservableList<ToDoListItem> deleteItem(ObservableList<ToDoListItem> toDelete, ObservableList<ToDoListItem> theList) {
+    public static ObservableList<ToDoListItem> initialItem(ObservableList<ToDoListItem> theList)
+    {
+        theList.add(new ToDoListItem("256 Characters", "Incomplete", time().toString()));
+        return theList;
+    }
+    public static ObservableList<ToDoListItem> addItem(String desc, String dueDate, String status, ObservableList<ToDoListItem> theList) {
+        theList.add(new ToDoListItem(desc,dueDate,status));
+        return theList;
+        //Will take in the incoming values and create an item
+        //Will add the item to the ObservableList of items
+
+    }
+
+    public static ObservableList<ToDoListItem> removeItem(ObservableList<ToDoListItem> toDelete, ObservableList<ToDoListItem> theList) {
+        for(ToDoListItem item: toDelete)
+        {
+            theList.remove(item);
+        }
         //loop to delete the item that was selected from the list passed in
         return theList;
     }
